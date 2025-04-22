5d5d03a5e41541cdec65ed95d2a28268472900fa
shour0 <shour908@gmail.com>
2025-04-22 22:22:45 +0300

    Fixed import

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
 import Model from "./components/Model"
